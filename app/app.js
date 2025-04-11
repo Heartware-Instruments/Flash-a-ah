2e3fb408ac89a61c812880f8d8b282461db08407
HeartWare Instruments <audiopieris@gmail.com>
2025-04-11 03:29:48 +0100

    Update app.js
    this is not for people to read, don't read this

--- app/app.js
+++ app/app.js
@@ -1,16 +1,6 @@
 // Generic Strings
 const root_url = "https://heartware-instruments.github.io/Flash-a-ah"
 
-
-// New changes involve reading from sources.json to find the 'sources' we should pull from
-// Those sources replace the previously hard coded 'examples.json' file, and should otherwise 
-// function the same.
-
-// The changes should primarily only affect gatherExampleData
-
-// When imported the examples will have the original data located in the .json file
-// as well as the 'source' field containing the data structure used to find the example
-
 var data = { 
     platforms: [],
     examples: [],
@@ -34,60 +24,6 @@ function getRootUrl() {
     var url = document.URL;
     return url;
 }
-
-// Reads the specified file containing JSON example meta-data
-// function gatherExampleData()
-// {
-//     // Get Source list as data 
-//     var self = this // assign self to 'this' before nested function calls...
-//     var src_url = getRootUrl().concat("data/sources.json") 
-//     var raw = new XMLHttpRequest();
-//     raw.open("GET", src_url, true);
-//     raw.responseType = "text"
-//     raw.onreadystatechange = function ()
-//     {
-//         if (this.readyState === 4 && this.status === 200) {
-//             var obj = this.response; 
-//             buffer = JSON.parse(obj);
-//             buffer.forEach( function(ex_src) {
-//                 // Launch another request with async function to load examples from the 
-//                 // specified urls 
-//                 // This will fill examples directly, and replace the importExamples/timeout situation.
-//                 var ext_raw = new XMLHttpRequest();
-//                 ext_raw.open("GET", ex_src.data_url, true);
-//                 ext_raw.responseType = "text"
-//                 ext_raw.onreadystatechange = function ()
-//                 {
-//                     if (this.readyState === 4 && this.status === 200) {
-//                         // Now this.response will contain actual example data 
-//                         var ext_obj = this.response;
-//                         ex_buffer = JSON.parse(ext_obj);
-//                         // Now we could just fill the examples data
-//                         // ex_buffer.forEach( function(ex_data) {
-//                         //     console.log("%s - %s", ex_src.name, ex_data.name);
-//                         // })
-//                         const unique_platforms = [...new Set(ex_buffer.map(obj => obj.platform))]
-//                         // This needs to be fixed to 'ADD' examples
-//                         //self.examples = data
-//                         self.examples.push(ex_buffer)
-//                         var temp_platforms = self.platforms.push(unique_platforms)
-
-//                         const new_platforms = [...new Set(temp_platforms.map(obj => obj))]
-//                         self.platforms = new_platforms
-//                     }
-//                 }
-//                 ext_raw.send(null)
-
-//                     // var self = this
-//                     // const unique_platforms = [...new Set(data.map(obj => obj.platform))] 
-//                     // self.examples = data
-//                     // self.platforms = unique_platforms
-//             })
-//         }
-//     }
-//     raw.send(null)
-// }
-
 
 function displayReadMe(fname)
 {
@@ -166,9 +102,12 @@ var app = new Vue({
         </b-form>
     </div>
     <b-row align="center" class="app_column">
+	<div class="logo-glitch">
+		<img src="data/HeartwareLogo.svg" alt="Heartware Instruments Logo" />
+	</div>
         <div>
-            <legend>Daisy Web Programmer</legend>
-            <p> Connect to the Daisy - If this is your first time here, follow the steps in Help section below </p>
+            <legend>Heartware Firmware Updator</legend>
+            <p> Connect to the LiveCut - If this is your first time here, follow the steps in Help section below </p>
             <p><b-button variant="es" id="connect"> Connect</b-button></p>
             <dialog id="interfaceDialog">
                 Your device has multiple DFU interfaces. Select one from the list below:
@@ -184,16 +123,14 @@ var app = new Vue({
                     <div class="nested_list">
                         <h2>Usage:</h2>
                         <ol>
-                            <li><p>Connect the Daisy to the Computer</p></li>
+                            <li><p>Connect LiveCut to the Computer</p></li>
                             <li><p>Enter the system bootloader by holding the BOOT button down, and then pressing, and releasing the RESET button.</p></li>
                             <li><p>Click the Connect button at the top of the page.</p></li>
                             <li><p>Select, "DFU in FS Mode"</p></li>
                             <li>
-                                <p>Now do either of the following:</p>
+                                <p>Now do the following:</p>
                                 <ul>
-                                    <li><p>Flash the blink example</p></li>
-                                    <li><p>Select a platform and an example from the drop down menu (descriptions, diagrams, etc. coming soon)</p></li>
-                                    <li><p>Click the Choose File button, and select the .bin file you would like to flash. This can be found in a projects "build" folder.</p></li>
+                                    <li><p>Select a module and firmware version</p></li>
                                 </ul>
                             </li>
                             <li><p>Click Program, and wait for the progress bar to finish.</p></li>
@@ -226,10 +163,6 @@ var app = new Vue({
         <b-row align="between">
             <b-col align="center" class="app_column">
                 <b-container>
-                    <b-row class="p-2">
-                        <legend>Getting Started? Flash the Blink example!</legend>
-                        <div><b-button variant="es" id="blink"  :disabled="no_device">Flash Blink!</b-button></div>
-                    </b-row>
                     <hr>
                     <b-row class="p-2">
                         <legend> Or select a platform and a program from the menu below.</legend>
